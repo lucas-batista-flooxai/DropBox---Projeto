0842e42f942bd2abfc20756497a23cbf2206da7a
Lucas <lucas.batista.flooxai@gmail.com>
2025-06-04 12:19:51 -0300

    feat: create and dispatch events

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -1,5 +1,8 @@
 class DropBoxController {
   constructor() {
+
+    this.onselectionchange = new Event('selectionchange')
+
     this.btnSendFileEl = document.querySelector("#btn-send-file");
     this.inputFilesEl = document.querySelector("#files");
     this.snackModalEl = document.querySelector("#react-snackbar-root");
@@ -29,6 +32,11 @@ class DropBoxController {
   }
 
   initEvents() {
+
+    this.listFilesEl.addEventListener('selectionchange', e => {
+      console.log('selectionchange')
+    })
+
     this.btnSendFileEl.addEventListener("click", (event) => {
       this.inputFilesEl.click();
     });
@@ -335,6 +343,11 @@ class DropBoxController {
 
   initEventsLi(li) {
     li.addEventListener("click", (e) => {
+
+      this.onselectionchange = new Event('selectionchange')
+
+      this.listFilesEl.dispatchEvent(this.onselectionchange)
+
       if (e.shiftKey) {
         let firstLi = this.listFilesEl.querySelector(".selected");
         if (firstLi) {
