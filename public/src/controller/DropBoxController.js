3d31d2ec51f0042fc18f034e53107b975a78c6d8
Lucas <lucas.batista.flooxai@gmail.com>
2025-06-04 14:10:24 -0300

    feat: creat folder

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -1,5 +1,7 @@
 class DropBoxController {
   constructor() {
+
+    this.currentFolder = ['hcode']
     this.onselectionchange = new Event("selectionchange");
 
     this.btnSendFileEl = document.querySelector("#btn-send-file");
@@ -55,6 +57,21 @@ class DropBoxController {
   }
 
   initEvents() {
+
+    this.btnNewFolder.addEventListener('click', e => {
+      
+      let name = prompt('Nome da nova pasta:')
+
+      if(name) {
+        this.getFirebaseRef().push().set({
+          name,
+          type: 'folder',
+          path: this.currentFolder.join('/')
+        })
+      }
+
+    })
+
     this.btnDelete.addEventListener("click", (e) => {
       this.removeTask()
         .then((responses) => {
