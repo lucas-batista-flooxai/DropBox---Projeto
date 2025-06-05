629fde9dc623fb9c87725f788cf0fe473642c847
Lucas <lucas.batista.flooxai@gmail.com>
2025-06-05 11:15:23 -0300

    feat: add rote for open file

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { error } = require("console");
 var express = require("express");
 var router = express.Router();
 var formidable = require("formidable");
@@ -6,6 +7,27 @@ var fs = require("fs");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+router.get("/file", (req, res) => {
+  let path = "./" + req.query.path;
+
+  if (fs.existsSync(path)) {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).json({
+          error: err,
+        });
+      } else {
+        res.status(200).end(data)
+      }
+    });
+  } else {
+    res.status(404).json({
+      error: "File not found",
+    });
+  }
 });
 
 router.delete("/file", (req, res) => {
@@ -28,6 +50,10 @@ router.delete("/file", (req, res) => {
             fields,
           });
         }
+      });
+    } else {
+      res.status(404).json({
+        error: "File not found",
       });
     }
   });
